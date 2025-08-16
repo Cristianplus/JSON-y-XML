5be8e9088756587ab37848df7535c0bb491ac2b5
Cristian Cordero <cristiancordero2810@gmail.com>
2025-08-16 04:09:18 -0500

    Ejercicio de práctica, donde se evidencia la manipulación de los ficheros XML y JSON

--- JSON y XML.py
+++ JSON y XML.py
@@ -16,10 +16,9 @@ json_file = "cristiandev.json"
 Ejercicio
 """
 
+# XML -------------------------------------------------------------
 
-# XML
-
-def save_xml():
+def create_xml():
 
     root = xml.Element("data")
 
@@ -34,14 +33,14 @@ def save_xml():
     tree = xml.ElementTree(root)
     tree.write(xml_file)
 
-save_xml()
+create_xml()
 
 with open(xml_file) as xml_data:
     print(xml_data.read())
 
-os.remove(xml_file)
+#os.remove(xml_file)          #Quitar "#" para accionar la función de eliminar
 
-#JSON
+#JSON ---------------------------------------------------------------
 
 def create_json():
     with open(json_file, "w") as json_data:
@@ -52,4 +51,45 @@ create_json()
 with open(json_file, "r") as json_data:
     print(json_data.read())
 
+#os.remove(json_file)         #Quitar "#" para accionar la función de eliminar
+
+"""
+Extra ------------------------------------------------------------------------
+"""
+
+create_json()
+create_xml()
+
+class Data:
+
+    def __init__(self, name, age, birth_date, programming_languages) -> None:
+        self.name = name
+        self.age = age
+        self.birth_date = birth_date
+        self.programming_languages = programming_languages
+
+with open(xml_file, "r") as xml_data:
+
+    root = xml.fromstring(xml_data.read())
+    name = root.find("name").text
+    age = root.find("age").text
+    birth_date = root.find("birth_date").text
+    programming_languages = []
+    for item in root.find("programming_languages"):
+        programming_languages.append(item.text)
+    
+    xml_class = Data(name, age, birth_date, programming_languages)
+    print(xml_class.__dict__)
+        
+with open(json_file, "r") as json_data:
+    json_dict = json.load(json_data)
+    json_class = Data(
+        json_dict["name"],
+        json_dict["age"],
+        json_dict["birth_date"],
+        json_dict["programming_languages"]
+    )
+    print(json_class.__dict__)
+
+#os.remove(xml_file)
 #os.remove(json_file)
